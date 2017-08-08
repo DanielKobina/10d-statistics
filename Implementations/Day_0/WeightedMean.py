80cb2d0278aa8c5a2b5337734d869bb32b9b2e8f
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-08-08 18:12:48 -0500

    Update WeightedMean.py

--- Implementations/Day_0/WeightedMean.py
+++ Implementations/Day_0/WeightedMean.py
@@ -1,3 +1,16 @@
+"""
+Objective
+In the previous challenge, we calculated a mean. In this challenge, we practice calculating a weighted mean. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given an array, , of integers and an array, , representing the respective weights of 's elements, calculate and print the weighted mean of 's elements. Your answer should be rounded to a scale of decimal place (i.e., format).
+
+Input Format
+The first line contains an integer, , denoting the number of elements in arrays and .
+The second line contains space-separated integers describing the respective elements of array .
+The third line contains space-separated integers describing the respective elements of array .
+"""
+
 N = int(input())
 X = input().split()
 W = input().split()
