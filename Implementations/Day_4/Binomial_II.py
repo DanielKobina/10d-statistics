72d2483777442690d69712e115cd17e4b1f28065
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-08-12 22:13:09 -0300

    Create Binomial_II.py

---
+++ Implementations/Day_4/Binomial_II.py
@@ -0,0 +1,67 @@
+"""
+Objective
+In this challenge, we go further with binomial distributions. We recommend reviewing the previous challenge's Tutorial before attempting this problem.
+
+Task
+A manufacturer of metal pistons finds that, on average, of the pistons they manufacture are rejected because they are incorrectly sized. What is the probability that a batch of pistons will contain:
+
+    No more than rejects?
+    At least rejects?
+
+Input Format
+
+A single line containing the following values (denoting the respective percentage of defective pistons and the size of the current batch of pistons):
+  12  10
+"""
+
+
+def factorial(val):
+    """factorial equation
+    input: value
+    output: factorial of value
+    
+    equation    val!"""
+    
+    if val <= 1:
+        return 1
+    ret = val * factorial(val-1)
+    return ret
+
+def combination(n,x): 
+    """combination equation
+    input: values n and r
+    output: combination of n and r
+    
+    equation        n!
+                  r!(n-1!)"""
+    
+    top = factorial(n)
+    bot = factorial(x) * factorial(n-x)
+    return top/bot
+
+def binomial(x,n,p):
+    """binomial mass distribution function
+    input: values x(number of successfull trials) n(number of trials), p(probability of successfull trial)
+    output: probability of function
+    
+    equation  (n;x) * (p**x) * (q**(n-x))"""
+
+    return (combination(n,x)) * (p**x) * ((1-p)**(n-x))
+
+p = .12
+x = 2
+n = 10
+
+one = 0
+for i in range(0,x+1): #Cumulative Distribution function
+    one += binomial(i,n,p) #probability mass function
+    
+two = 0
+for i in range(x,n+1): #Cumulative Distribution function
+    two += binomial(i,n,p) #probability mass function
+    
+one = "{:.3f}".format(one)
+print(one) 
+
+two = "{:.3f}".format(two)
+print(two) 
