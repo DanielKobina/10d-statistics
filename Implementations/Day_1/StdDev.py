f0aa33b9d988f46bb5a60a5e3f252a750ac72bf9
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-09-30 12:21:59 -0400

    Update StdDev.py

--- Implementations/Day_1/StdDev.py
+++ Implementations/Day_1/StdDev.py
@@ -1,21 +1,23 @@
 """
-Objective
-In this challenge, we practice calculating standard deviation. Check out the Tutorial tab for learning materials and an instructional video!
+Objective:
+In this challenge, we practice calculating standard deviation. 
+    Check out the Tutorial tab for learning materials and an instructional video!
 
-Task
-Given an array, , of integers, calculate and print the standard deviation. Your answer should be in decimal form, rounded to a scale of decimal place (i.e., format). An error margin of will be tolerated for the standard deviation.
+Task:
+Given an array, X, of N integers, calculate and print the standard deviation. 
+    Your answer should be in decimal form, rounded to a scale of decimal place (i.e., 12.3 format). 
+    An error margin of +/- 1 will be tolerated for the standard deviation.
 
-Input Format
-
-The first line contains an integer, , denoting the number of elements in the array.
-The second line contains space-separated integers describing the respective elements of the array.
+Input Format:
+The first line contains an integer, N, denoting the number of elements in the array.
+    The second line contains N space-separated integers describing the respective elements of the array.
 """
 
-N = int(input())
-A = input().split()
-A = [int(x) for x in A] #change from strings to integers
+N = int(input()) #Number of elements in array
+A = input().split() #Array of elements
+A = [int(x) for x in A] #Change array from strings to integers
 
-avg = sum(A)/N #average
+avg = sum(A)/N #Average of array
 chg = 0
 
 for ele in A:
