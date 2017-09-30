7b6a4a8c96a8aedfdea0291c9734abfbb5bd7010
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-09-30 13:26:11 -0400

    Update Binomial_II.py

--- Implementations/Day_4/Binomial_II.py
+++ Implementations/Day_4/Binomial_II.py
@@ -1,64 +1,66 @@
 """
-Objective
-In this challenge, we go further with binomial distributions. We recommend reviewing the previous challenge's Tutorial before attempting this problem.
+Objective:
+In this challenge, we go further with binomial distributions. 
+    We recommend reviewing the previous challenge's Tutorial before attempting this problem.
 
-Task
-A manufacturer of metal pistons finds that, on average,12% of the pistons they manufacture are rejected because they are incorrectly sized. 
-What is the probability that a batch of 10 pistons will contain:
-    No more than 2 rejects?
-    At least 2 rejects?
+Task:
+A manufacturer of metal pistons finds that, on average, 12% of the pistons they manufacture 
+    are rejected because they are incorrectly sized. 
+    What is the probability that a batch of 10 pistons will contain:
+        -No more than 2 rejects?
+        -At least 2 rejects?
 
-Input Format
-
+Input Format:
 A single line containing the following values (denoting the respective percentage of defective pistons and the size of the current batch of pistons):
   12  10
 """
 
-
 def factorial(val):
-    """factorial equation
-    input: value
-    output: factorial of value
+    """Factorial equation -> val!
+    Input: value (Integer value)
+    Output: Factorial of given value"""
     
-    equation    val!"""
-    
-    if val <= 1:
+    if val <= 1: #Factorial of 0 and 1 equal 1, negative values not considered in problem
         return 1
-    ret = val * factorial(val-1)
+      
+    ret = val * factorial(val-1) #Recursive call
     return ret
 
 def combination(n,x): 
-    """combination equation
-    input: values n and r
-    output: combination of n and r
-    
-    equation        n!
-                  r!(n-1!)"""
+    """Combination equation:
+                    n!
+                  x!(n-x!)
+                  
+    Input: n (number of trials)
+           r (number of choices)
+    Output: Combination of n and x"""
     
     top = factorial(n)
     bot = factorial(x) * factorial(n-x)
     return top/bot
 
 def binomial(x,n,p):
-    """binomial mass distribution function
-    input: values x(number of successfull trials) n(number of trials), p(probability of successfull trial)
-    output: probability of function
+    """Binomial Mass Distribution function
+    equation:  (n choose x) * (p ^ x) * (q ^ (n-x))
     
-    equation  (n;x) * (p**x) * (q**(n-x))"""
+    Input: x (number of successfull trials) 
+           n (number of trials)
+           p (probability of successfull trial)
+    Output: probability of function"""
 
     return (combination(n,x)) * (p**x) * ((1-p)**(n-x))
 
-p = .12 #probability a manufactured piston is rejected
-x = 2 #aimed number of pistons
-n = 10 #total batch of pistons
+p = .12 #Probability a manufactured piston is rejected
+x = 2 #Aimed number of pistons
+n = 10 #Total number of pistons
 
 one = 0
 for i in range(0,x+1): #Cumulative Distribution function
-    one += binomial(i,n,p) #probability mass function
+    one += binomial(i,n,p) #Mass Distribution function
     
 two = 0
 for i in range(x,n+1): #Cumulative Distribution function
-    two += binomial(i,n,p) #probability mass function
+    two += binomial(i,n,p) #Mass Distribution function
     
 one = "{:.3f}".format(one)
 print(one) 
