f6f78698f28e93c4f4fc7b2c2811c3143fcce360
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-09-30 12:19:00 -0400

    Update Quartiles.py

--- Implementations/Day_1/Quartiles.py
+++ Implementations/Day_1/Quartiles.py
@@ -1,36 +1,41 @@
 """
-Objective
-In this challenge, we practice calculating quartiles. Check out the Tutorial tab for learning materials and an instructional video!
+Objective:
+In this challenge, we practice calculating quartiles. 
+    Check out the Tutorial tab for learning materials and an instructional video!
 
-Task
-Given an array, , of integers, calculate the respective first quartile (), second quartile (), and third quartile (). It is guaranteed that , , and are integers.
+Task:
+Given an array, X, of n integers, calculate the respective first quartile (Q1), 
+    second quartile (Q2), and third quartile (Q3). It is guaranteed that Q1, Q2, and Q3 are integers.
 
-Input Format
-
-The first line contains an integer, , denoting the number of elements in the array.
-The second line contains space-separated integers describing the array's elements.
+Input Format:
+The first line contains an integer, n, denoting the number of elements in the array.
+    The second line contains n space-separated integers describing the array's elements.
 """
 
 def median_finder(A,N):
-
-    if N%2 == 0: #even
-        halfN = int(N/2)
-        
+    """
+    Median of array A
+    Input: A (array containing numeric values)
+           N (length of the array)
+    Output: The median of array A"""
+    
+    halfN = int(N/2)
+    
+    if N%2 == 0: #Even array length
         h1 = A[halfN-1] #Python uses 0 indexing
         h2 = A[halfN]
         mid = (h1+h2)/2
 
-    else: #odd
-        halfN = int(N/2)
+    else: #Odd array length
         mid = A[halfN]
     
     return mid
 
-N = int(input()) #number of elements in array
-A = input().split() #array
+N = int(input()) #Number of elements in array
+A = input().split() #Array of elements
 
-A = [int(x) for x in A] #integers
-A = sorted(A) #sort array
+A = [int(x) for x in A] #Change array from strings to integers
+A = sorted(A) #Sort array
 
 mid = median_finder(A,N) #Q2, after finding mid, split up array into upper and lower
 
