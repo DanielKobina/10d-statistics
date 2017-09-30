6e1d52b25bdba4ca2187852dac65d69c118cb8db
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-09-30 12:13:37 -0400

    Update IQR.py

--- Implementations/Day_1/IQR.py
+++ Implementations/Day_1/IQR.py
@@ -1,66 +1,66 @@
 """
-Objective
-In this challenge, we practice calculating the interquartile range. We recommend you complete the Quartiles challenge before attempting this problem.
+Objective:
+In this challenge, we practice calculating the interquartile range. 
+    We recommend you complete the Quartiles challenge before attempting this problem.
 
-Task
-The interquartile range of an array is the difference between its first () and third () quartiles (i.e., ).
+Task:
+The interquartile range of an array is the difference between its first (Q1) and third (Q3) quartiles (i.e., Q3 - Q1).
 
-Given an array, , of integers and an array, , representing the respective frequencies of 's elements, construct a data set, , where each occurs at frequency . Then calculate and print 's interquartile range, rounded to a scale of decimal place (i.e., format).
+Given an array, X, of n integers and an array, F, representing the respective frequencies of X's elements, 
+    construct a data set, S, where each xi occurs at frequency fi. 
+    Then calculate and print S's interquartile range, rounded to a scale of 1 decimal place (i.e., 12.3 format).
 
-Tip: Be careful to not use integer division when averaging the middle two elements for a data set with an even number of elements, and be sure to not include the median in your upper and lower data sets.
+Tip: 
+Be careful to not use integer division when averaging the middle two elements for a data set with an even number of elements, 
+    and be sure to not include the median in your upper and lower data sets.
 
-Input Format
-
-The first line contains an integer, , denoting the number of elements in arrays and .
-The second line contains space-separated integers describing the respective elements of array .
-The third line contains space-separated integers describing the respective elements of array .
+Input Format:
+The first line contains an integer, n, denoting the number of elements in arrays X and F.
+    The second line contains n space-separated integers describing the respective elements of array X.
+    The third line contains n space-separated integers describing the respective elements of array F.
 """
 
 def medianF(N,A):
-    '''
-    finds median of array
-    input: N,number of elements in array
-           A, array of integers
-    return: median of array
-    '''
-    if N%2 == 0: #even
-        h1 = int(N/2)-1
-        h2 = int(N/2)
-        
-        v1 = A[h1]
-        v2 = A[h2]
-        
-        mid = (v1+v2)/2
-        return mid
+    """
+    Median of array A
+    Input: A (array containing numeric values)
+           N (length of the array)
+    Output: The median of array A"""
     
-    else: #odd
-        halfN = int(N/2)
-        mid = A[halfN]
-        return mid
+    sortA = sorted(A)
+    halfN = int(N/2)
+
+    if N%2 == 0: #Even number of elements
+        h1 = halfN-1 #Python uses 0 indexing, so half-1 and half give median
+        h2 = halfN
+        return (sortA[h1] + sortA[h2])/2
+    
+    else: #Odd number of elements
+        return sortA[halfN-1]
 
 N = int(input())
-A = input().split() #create array from string
+A = input().split() #Create array from string
 F = input().split()
 
-A = [int(item) for item in A] #change value and frequency to integers
+A = [int(item) for item in A] #Change value and frequency to integers
 F = [int(item) for item in F]
 
 L = [] #list with values appearing in correspondence to their frequencies
-count = 0 #number of elements in list L
+count = 0 #Number of elements in list L
 
 for i in range(N):
-    for j in range(F[i]): #append to list the number of times value appears as frequency
+    for j in range(F[i]): #Append to list the number of times value appears as frequency
         L.append(A[i])
         count += 1
 
 OrdL = sorted(L)
 halfN = int(count/2)
 
-if N%2 == 0: #even
+if N%2 == 0: #Even array length
     low = OrdL[:halfN]
     high = OrdL[halfN:]
     
-else: #odd
+else: #Odd array length
     halfN += 1
     
     low = OrdL[:halfN]
