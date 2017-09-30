7ded3cc1d0345fbd6b0c3eab5b38c33b8775cf2c
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-09-30 12:01:26 -0400

    Update WeightedMean.py

--- Implementations/Day_0/WeightedMean.py
+++ Implementations/Day_0/WeightedMean.py
@@ -1,29 +1,31 @@
 """
-Objective
-In the previous challenge, we calculated a mean. In this challenge, we practice calculating a weighted mean. Check out the Tutorial tab for learning materials and an instructional video!
+Objective:
+In the previous challenge, we calculated a mean. In this challenge, we practice calculating a weighted mean.
+    Check out the Tutorial tab for learning materials and an instructional video!
 
-Task
-Given an array, , of integers and an array, , representing the respective weights of 's elements, calculate and print the weighted mean of 's elements. Your answer should be rounded to a scale of decimal place (i.e., format).
+Task:
+Given an array, X, of N integers and an array, W, representing the respective weights of X's elements, 
+    calculate and print the weighted mean of X's elements. 
+    Your answer should be rounded to a scale of decimal place (i.e., 12.3 format).
 
-Input Format
-The first line contains an integer, , denoting the number of elements in arrays and .
-The second line contains space-separated integers describing the respective elements of array .
-The third line contains space-separated integers describing the respective elements of array .
+Input Format:
+The first line contains an integer, N, denoting the number of elements in arrays X and W.
+    The second line contains N space-separated integers describing the respective elements of array X.
+    The third line contains N space-separated integers describing the respective elements of array W.
 """
 
 N = int(input())
 X = input().split()
 W = input().split()
 
-X = [int(c) for c in X] #change from strings to integers
+X = [int(c) for c in X] #Change from strings to integers
 W = [int(c) for c in W]
 
 top = 0
 bot = 0
 
-for i in range(N): #weighted mean formula
+for i in range(N): #Weighted mean formula
     top += X[i] * W[i] 
     bot += W[i]
    
-#print(float(int(top/bot)))
 print("{:.1f}".format(top/bot)) #1 decimal point
