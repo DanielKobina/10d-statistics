db9195ee5831d5b99a3a99f2e9e6c1e428a671b0
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-10-01 11:19:36 -0400

    Update Geometric_I.py

--- Implementations/Day_4/Geometric_I.py
+++ Implementations/Day_4/Geometric_I.py
@@ -5,7 +5,7 @@ In this challenge, we go further with geometric distributions.
 
 Task:
 The probability that a machine produces a defective product is 1/3. 
-    What is the probability that the 1st defect is found during the **5th** inspection?
+    What is the probability that the 1st defect is found during the 5th inspection?
 
 Input Format:
 The first line contains the respective space-separated numerator and denominator for the probability of a defect, 
@@ -13,6 +13,7 @@ The first line contains the respective space-separated numerator and denominator
     1 3
     5
 """
+
 def geometric(p,k):
     """Geometric Mass Distribution function
     equation: [q ^ (k-1)] * p
