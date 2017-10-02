d7cdd62e965ae2fe595918ff84416597d38f23ce
Daniel Kobina Ofori-Dankwa <kobinaoforidankwa@gmail.com>
2017-10-01 22:20:03 -0400

    Create Pearson_CC.py

---
+++ Implementations/Day_7/Pearson_CC.py
@@ -0,0 +1,14 @@
+"""
+Objective:
+In this challenge, we practice calculating the Pearson correlation coefficient. Check out the Tutorial tab for learning materials!
+
+Task:
+Given two -element data sets, and , calculate the value of the Pearson correlation coefficient.
+
+Input Format:
+The first line contains an integer, , denoting the size of data sets and .
+  The second line contains space-separated real numbers (scaled to at most one decimal place), defining data set .
+  The third line contains space-separated real numbers (scaled to at most one decimal place), defining data set .
+"""
+
+﻿
